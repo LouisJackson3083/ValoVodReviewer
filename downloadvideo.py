8444c83289117af9dd614efb22554315832a0d25
Louis Jackson <louis.jackson@student.manchester.ac.uk>
2023-09-24 14:42:15 +0100

    Added optimal quality video download and cropping

--- downloadvideo.py
+++ downloadvideo.py
@@ -1,21 +1,9 @@
 from pytube import YouTube
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.editor import *
 import sys
-import os.path
-
-# # Download vod
-# url = 'https://www.youtube.com/watch?v=q4LQdAVusr0&list=PLMJ9cfx_WDdybdHDw28uCEFnxQvprJ59W&t=1180s'
-# vod = YouTube(url).streams.get_highest_resolution()
-# path_to_vod = './vods/'+str(vod.title)
-# if (os.path.isfile(path_to_vod)):
-#     print("Vod already exists!")
-#     exit
-
-# vod.download(output_path=f'./vods')
-
-# vod_start = 1180
-# vod_end = 1300
-# ffmpeg_extract_subclip(path_to_vod, vod_start, vod_end, path_to_vod)
+import os
+import asyncio
 
 def on_progress(stream, chunk, bytes_remaining):
     total_size = stream.filesize
@@ -23,10 +11,20 @@ def on_progress(stream, chunk, bytes_remaining):
     percentage_of_completion = bytes_downloaded / total_size * 100
     print(percentage_of_completion)
 
-def download_video(url: str="https://www.youtube.com/watch?v=99k-EAMBuM8", verbose: bool=False):
-    chunk_size = 1024
+async def download_vod(url: str="https://www.youtube.com/watch?v=99k-EAMBuM8", verbose: bool=False) -> str:
+    """
+    Given a url, we download a video to ./vods/
+    returns:
+        video path
+        video resolution
+    """
+    chunk_size = 2048
     yt = YouTube(url)
-    vod = yt.streams.get_highest_resolution()
+
+    # find the highest quality mp4 vod
+    itag = yt.streams.filter(file_extension='mp4').order_by('resolution').desc()[0].itag
+    vod = yt.streams.get_by_itag(itag)
+
     yt.register_on_progress_callback(on_progress)
     if (verbose):
         print(f"Fetching \"{vod.title}\"..")
@@ -39,5 +37,23 @@ def download_video(url: str="https://www.youtube.com/watch?v=99k-EAMBuM8", verbo
 
         print(f"Downloading \"{vod.title}\"..")
     vod.download(output_path='./vods/')
+    return './vods/'+str(vod.title), int(vod.resolution[:-1])
 
-download_video(url="https://www.youtube.com/watch?v=99k-EAMBuM8")
+async def crop_vod_to_minimap(vod_path: str, vod_resolution: int):
+    vod_height = vod_resolution
+    vod_width = vod_resolution*1.77777777778
+
+    clip = VideoFileClip(vod_path+'.mp4')
+    clip = clip.subclip(2000, 2100)
+    clip = clip.crop(x1=0, y1=0, x2=vod_width/4.5, y2=vod_height/2.5)
+    clip.write_videofile(vod_path+'_EDITED.mp4')
+    clip.close()
+
+
+async def main() -> None:
+    vod_path, vod_resolution = await download_vod(url="https://www.youtube.com/watch?v=r8aLw3nt9zE&list=PLMJ9cfx_WDdybdHDw28uCEFnxQvprJ59W&index=20", verbose=True)
+    await crop_vod_to_minimap(vod_path=vod_path, vod_resolution=vod_resolution)
+    os.remove(vod_path+'.mp4')
+
+if __name__ == '__main__':
+    asyncio.run(main())
