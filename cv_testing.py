29646bc02a1e9b0da0b4ab41cf3d174547bea1f1
Louis Jackson <louis.jackson@student.manchester.ac.uk>
2023-09-28 13:50:51 +0100

    improved flex with color hue limits

--- cv_testing.py
+++ cv_testing.py
@@ -9,37 +9,73 @@ class ChangeEffects():
     saved_saturation_amt = 3
     saved_dilate_amt = 3
     saved_threshold_limit_dist = 100
+    saved_lower_color_limit = 0
+    saved_upper_color_limit = 360
 
     def change_blur_amt(self, val):
+        self.saved_blur_amt = (val*2) + 1
         self.add_effects(
-            blur_amt=(val*2)+1,
+            blur_amt=self.saved_blur_amt,
             saturation_amt=self.saved_saturation_amt,
             dilate_amt=self.saved_dilate_amt,
             threshold_limit_dist=self.saved_threshold_limit_dist,
+            lower_color_limit = self.saved_lower_color_limit,
+            upper_color_limit = self.saved_upper_color_limit,
         )
 
     def change_saturation_amt(self, val):
+        self.saved_saturation_amt = val
         self.add_effects(
             blur_amt=self.saved_blur_amt,
-            saturation_amt=val,
+            saturation_amt=self.saved_saturation_amt,
             dilate_amt=self.saved_dilate_amt,
             threshold_limit_dist=self.saved_threshold_limit_dist,
+            lower_color_limit = self.saved_lower_color_limit,
+            upper_color_limit = self.saved_upper_color_limit,
         )
     
     def change_dilate_amt(self, val):
-        self.add_effects(
-            blur_amt=self.saved_blur_amt,
-            saturation_amt=self.saved_saturation_amt,
-            dilate_amt=(val*2)+1,
-            threshold_limit_dist=self.saved_threshold_limit_dist,
-        )
-
-    def change_threshold_limit_dist(self, val):
+        self.saved_dilate_amt = (val*2) + 1
         self.add_effects(
             blur_amt=self.saved_blur_amt,
             saturation_amt=self.saved_saturation_amt,
             dilate_amt=self.saved_dilate_amt,
-            threshold_limit_dist=val,
+            threshold_limit_dist=self.saved_threshold_limit_dist,
+            lower_color_limit = self.saved_lower_color_limit,
+            upper_color_limit = self.saved_upper_color_limit,
+        )
+
+    def change_threshold_limit_dist(self, val):
+        self.saved_threshold_limit_dist = val
+        self.add_effects(
+            blur_amt=self.saved_blur_amt,
+            saturation_amt=self.saved_saturation_amt,
+            dilate_amt=self.saved_dilate_amt,
+            threshold_limit_dist=self.saved_threshold_limit_dist,
+            lower_color_limit = self.saved_lower_color_limit,
+            upper_color_limit = self.saved_upper_color_limit,
+        )
+
+    def change_lower_color_limit(self, val):
+        self.saved_lower_color_limit = val
+        self.add_effects(
+            blur_amt=self.saved_blur_amt,
+            saturation_amt=self.saved_saturation_amt,
+            dilate_amt=self.saved_dilate_amt,
+            threshold_limit_dist=self.saved_threshold_limit_dist,
+            lower_color_limit = self.saved_lower_color_limit,
+            upper_color_limit = self.saved_upper_color_limit,
+        )
+
+    def change_upper_color_limit(self, val):
+        self.saved_lower_color_limit = val
+        self.add_effects(
+            blur_amt=self.saved_blur_amt,
+            saturation_amt=self.saved_saturation_amt,
+            dilate_amt=self.saved_dilate_amt,
+            threshold_limit_dist=self.saved_threshold_limit_dist,
+            lower_color_limit = self.saved_lower_color_limit,
+            upper_color_limit = self.saved_upper_color_limit,
         )
 
     def add_effects(
@@ -48,13 +84,11 @@ class ChangeEffects():
             saturation_amt: int,
             dilate_amt: int,
             threshold_limit_dist: int,
+            lower_color_limit: int,
+            upper_color_limit: int,
             ):
         
         frame_copy = frame.copy()
-        self.saved_blur_amt = blur_amt
-        self.saved_saturation_amt = saturation_amt
-        self.saved_dilate_amt = dilate_amt
-        self.saved_threshold_limit_dist = threshold_limit_dist
 
         # Blur
         frame_copy = cv2.blur(frame_copy, (blur_amt, blur_amt))
@@ -67,8 +101,8 @@ class ChangeEffects():
         frame_copy = cv2.merge([h,s,v])
 
         # Threshold color
-        lower_limit = np.array([0, (255-threshold_limit_dist), (255-threshold_limit_dist)])
-        upper_limit = np.array([360, 255, 255])
+        lower_limit = np.array([lower_color_limit, (255-threshold_limit_dist), (255-threshold_limit_dist)])
+        upper_limit = np.array([upper_color_limit, 255, 255])
         frame_copy = cv2.inRange(frame_copy, lower_limit, upper_limit)
 
         # Dilate amount
@@ -86,6 +120,8 @@ class ChangeEffects():
         cv2.createTrackbar('saturation_amt', windowName, 0, 100, self.change_saturation_amt)
         cv2.createTrackbar('dilate_amt', windowName, 0, 100, self.change_dilate_amt)
         cv2.createTrackbar('threshold_limit_dist', windowName, 0, 255, self.change_threshold_limit_dist)
+        cv2.createTrackbar('lower_color_limit', windowName, 0, 360, self.change_lower_color_limit)
+        cv2.createTrackbar('upper_color_limit', windowName, 0, 360, self.change_upper_color_limit)
 
         cv2.waitKey(0)
         cv2.destroyAllWindows()
